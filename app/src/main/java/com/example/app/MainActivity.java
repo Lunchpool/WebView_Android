b221ce49291c4b477801d62221822f7fc6f554ec
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-04-20 15:19:16 -0400

    add current project webview

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
         webSettings.setJavaScriptEnabled(true);
 
         // Use remote resource
-        mWebView.loadUrl("http://example.com");
+        mWebView.loadUrl("https://unf-swuber.herokuapp.com");
 
         // Stop local links and redirects from opening in browser instead of WebView
         mWebView.setWebViewClient(new MyAppWebViewClient());
